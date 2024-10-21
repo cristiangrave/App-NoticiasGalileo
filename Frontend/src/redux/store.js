0611a90904d4548af0fdcb71969df602fa7e31da
cristiangrave <crisg5284@gmail.com>
2024-10-21 09:31:00 -0600

    tercerCommit

--- Frontend/src/redux/store.js
+++ Frontend/src/redux/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import optionReducer from "./reducers/optionSlice";
+import CreateSliceOP from "./reducers/createSlice";
 
 export default configureStore({
   reducer: {
     option: optionReducer,
+    createOp: CreateSliceOP,
   },
 });
