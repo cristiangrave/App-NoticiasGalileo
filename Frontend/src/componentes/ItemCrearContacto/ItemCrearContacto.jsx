96bfbe0b909d1a876d4ac5021aa4a2c1e1424a90
cristiangrave <crisg5284@gmail.com>
2024-10-22 15:22:42 -0600

    ItemCrearContactoCambios

--- Frontend/src/componentes/ItemCrearContacto/ItemCrearContacto.jsx
+++ Frontend/src/componentes/ItemCrearContacto/ItemCrearContacto.jsx
@@ -11,48 +11,38 @@ const ItemCrearContacto = () => {
           <Row>
             <Col
               xs={12}
-              md={2}
+              md={6}
               className="d-flex justify-content-center align-items-center">
               <Row>
-                {/* Ícono de imagen */}
-                <CardImg src="/icono-agregar-imagen.png" />
+                <CardImg
+                  src="/icono-agregar-imagen.png"
+                  onClick={() => {
+                    console.log("click en imagen hacer algo como");
+                  }}
+                />
               </Row>
+            </Col>
+            <Col xs={12} md={6} className="mb-3">
+              <Form.Group controlId="formTitle" className="mb-3">
+                <Form.Label>Nombre Contacto</Form.Label>
+                <Form.Control type="text" placeholder="Nombre Contacto" />
+              </Form.Group>
+              <Form.Group controlId="formStatus" className="mb-3">
+                <Form.Label>Estado</Form.Label>
+                <Form.Select>
+                  <option>Seleccionar...</option>
+                </Form.Select>
+              </Form.Group>
+              <Form.Group controlId="formCategory" className="mb-3">
+                <Form.Label>Puesto</Form.Label>
+                <Form.Control type="text" placeholder="Puesto que Desempeña" />
+              </Form.Group>
             </Col>
           </Row>
 
-          <Col xs={12} md={10}>
+          <Col xs={12} md={12}>
             <Row>
               <Col xs={12} md={6} className="mb-3">
-                {/* Campo de Título */}
-                <Form.Group controlId="formTitle">
-                  <Form.Label>Nombre Contacto</Form.Label>
-                  <Form.Control type="text" placeholder="Nombre Contacto" />
-                </Form.Group>
-              </Col>
-
-              <Col xs={12} md={6} className="mb-3">
-                {/* Campo de Estado */}
-                <Form.Group controlId="formStatus">
-                  <Form.Label>Estado</Form.Label>
-                  <Form.Select>
-                    <option>Seleccionar...</option>
-                  </Form.Select>
-                </Form.Group>
-              </Col>
-
-              <Col xs={12} md={6} className="mb-3">
-                {/* Campo de Categoría */}
-                <Form.Group controlId="formCategory">
-                  <Form.Label>Puesto</Form.Label>
-                  <Form.Control
-                    type="text"
-                    placeholder="Puesto que Desempeña"
-                  />
-                </Form.Group>
-              </Col>
-
-              <Col xs={12} md={6} className="mb-3">
-                {/* Campo de Fecha de Publicación */}
                 <Form.Group controlId="formDate">
                   <Form.Label>Correo Electronico</Form.Label>
                   <Form.Control
@@ -61,19 +51,14 @@ const ItemCrearContacto = () => {
                   />
                 </Form.Group>
               </Col>
+              <Col xs={12} md={6} className="mb-3">
+                <Form.Group controlId="formCareer">
+                  <Form.Label>Telefono</Form.Label>
+                  <Form.Control type="email" placeholder="Telefono" />
+                </Form.Group>
+              </Col>
             </Row>
           </Col>
-
-          <Row>
-            <Col xs={12} md={6} className="mb-3">
-              {/* Campo de Carrera */}
-              <Form.Group controlId="formCareer">
-                <Form.Label>Telefono</Form.Label>
-                <Form.Control type="email" placeholder="Telefono" />
-              </Form.Group>
-            </Col>
-          </Row>
-
           <Row className="mt-2">
             <Col className="d-flex justify-content-end">
               {/* Botones de Cancelar y Actualizar */}
