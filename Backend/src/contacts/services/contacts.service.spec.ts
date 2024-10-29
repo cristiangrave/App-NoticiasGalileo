8fb22fca899d3f069de11bd3a17de3d4a9c17f16
cristiangrave <crisg5284@gmail.com>
2024-10-29 14:14:31 -0600

    HaciendoAjustesBackend

--- Backend/src/contacts/services/contacts.service.spec.ts
+++ Backend/src/contacts/services/contacts.service.spec.ts
@@ -1,15 +1,7 @@
-<<<<<<< HEAD
-import { Test, TestingModule } from '@nestjs/testing';
-import { ContactsService } from './contacts.service';
-
-describe('ContactsService', () => {
-=======
 import { Test, TestingModule } from "@nestjs/testing";
 import { ContactsService } from "./contacts.service";
-import { beforeEach } from "node:test";
 
 describe("ContactsService", () => {
->>>>>>> c2045de93eaebde71f8e98a0bc3a0bebbdc894a7
   let service: ContactsService;
 
   beforeEach(async () => {
@@ -20,11 +12,7 @@ describe("ContactsService", () => {
     service = module.get<ContactsService>(ContactsService);
   });
 
-<<<<<<< HEAD
-  it('should be defined', () => {
-=======
   it("should be defined", () => {
->>>>>>> c2045de93eaebde71f8e98a0bc3a0bebbdc894a7
     expect(service).toBeDefined();
   });
 });
