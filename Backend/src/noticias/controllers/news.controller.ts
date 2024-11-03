ae1be7d5f6161c53f41c087ee9ace4dadae572a1
Micker <carbonellerick811@gmail.com>
2024-11-02 22:51:28 -0600

    Module: Noticias Correction: Eliminacion de dtos repetidos y de nombre dto de noticia en Noticias New Dependency: Multer

--- Backend/src/noticias/controllers/news.controller.ts
+++ Backend/src/noticias/controllers/news.controller.ts
@@ -7,22 +7,30 @@ import {
   HttpCode,
   Post,
   Body,
-  Put
+  Put,
+  UseInterceptors,
+  UploadedFile
 } from '@nestjs/common';
 import { NoticiaService, Noticia } from '../services/noticias.service';
-import { CreateNewsDto } from './createNews.dto';
-import { UpdateNewsDto } from './updateNews.dto';
+import { CreateNewsDto } from '../dtos/createNews.dto';
+import { UpdateNewsDto } from '../dtos/updateNews.dto';
+import { UploadService } from '../services/upload.service';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { diskStorage } from 'multer';
+import { extname } from 'path';
 
 /*nombre del enpoint */
 @Controller('noticiasEstudiantes')
 export class NoticiasController {
-  constructor(private readonly noticiasService: NoticiaService) {}
+  constructor(
+    private readonly noticiasService: NoticiaService, 
+    private readonly uploadService: UploadService) {}
 
   // Endpoint para obtener todos los contactos
   @Get()
   @HttpCode(HttpStatus.OK) // Código de estado 200
-  findAll(): { statusCode: number; message: string; data: Noticia[] } {
-    const noticias = this.noticiasService.findAll();
+  async findAll(): Promise<{ statusCode: number; message: string; data: Noticia[] }> {
+    const noticias = await this.noticiasService.findAll();
     return {
       statusCode: HttpStatus.OK,
       message: 'Noticias Obtenidas Exitosamente',
@@ -32,12 +40,8 @@ export class NoticiasController {
 
   // Endpoint para obtener un contacto por ID
   @Get(':id')
-  findOne(@Param('id') id: string): {
-    statusCode: number;
-    message: string;
-    data?: Noticia;
-  } {
-    const noticia = this.noticiasService.findOne(+id);
+  async findOne(@Param('id') id: string): Promise<{ statusCode: number; message: string; data?: Noticia }> {
+    const noticia = await this.noticiasService.findOne(id);
     if (!noticia) {
       throw new NotFoundException(
         `No se encontró información para la Noticia con ID ${id}`,
@@ -52,13 +56,72 @@ export class NoticiasController {
 
    // Endpoint para Crear un contacto
    @Post()
-   create(@Body() createNewsDto: CreateNewsDto){
-     return this.noticiasService.create(createNewsDto);
+   @UseInterceptors(FileInterceptor('imagen', {
+    storage: diskStorage({
+      destination: './uploads',
+      filename: (req, file, cb) => {
+        // Generar un nombre único para la imagen
+        const randomName = Array(32).fill(null).map(() => (Math.round(Math.random() * 16)).toString(16)).join('');
+        const fileExt = extname(file.originalname);  // Obtener la extensión del archivo original
+        cb(null, `${randomName}${fileExt}`);  // Generar el nombre completo del archivo
+      }, 
+    }), 
+    fileFilter: (req, file, cb) => {
+      if(!file.originalname.match(/\.(jpg|jpeg|png)$/)){
+        return new Error('Formato de imagen Invalido')
+      } else {
+        return console.log('Formato de imagen valido')
+      }
+  }
+   }))
+   async create(@Body() createNewsDto: CreateNewsDto, @UploadedFile() file: Express.Multer.File): Promise<{ statusCode: number; message: string; data: Noticia }> {
+     
+    // Subir la imagen si es necesario
+    const hayImagen = this.uploadService.uploadImage(file);
+    
+    // Creacion de la noticia con datos y la imagen convertida a String
+    const noticia = await this.noticiasService.create({...createNewsDto, imagen: hayImagen});
+
+    return {
+      statusCode: HttpStatus.CREATED,
+      message: 'Noticia creada exitosamente',
+      data: noticia
+    }
    }
  
    // Endpoint para Editar un Contacto
    @Put(':id')
-   updateNews(@Param('id') id: number, @Body() updateNewsDto:UpdateNewsDto) {
-     return this.noticiasService.update(id, updateNewsDto)
+   @UseInterceptors(FileInterceptor('imagen', {
+    storage: diskStorage({
+      destination: './uploads',
+      filename: (req, file, cb) => {
+        const randomName = Array(32)
+          .fill(null)
+          .map(() => (Math.round(Math.random() * 16)).toString(16))
+          .join('');
+        const fileExt = extname(file.originalname);
+        cb(null, `${randomName}${fileExt}`);
+      },
+    })
+   }))
+   async updateNews(@Param('id') id: string, @Body() updateNewsDto:UpdateNewsDto, @UploadedFile() file?: Express.Multer.File):  Promise<{ statusCode: number; message: string; data: Noticia }> {
+
+    //Subir la imagen si se da
+    let imagen;
+
+    if (file) {
+      imagen = this.uploadService.uploadImage(file);
+    }
+
+    const noticia = await this.noticiasService.update(id,{...updateNewsDto, ...(imagen && { imagen })});
+    if(!noticia) {
+      throw new NotFoundException(`No se encontró información para la Noticia con ID ${id}`);
+    }
+
+    return {
+      statusCode: HttpStatus.OK,
+      message: 'Noticia actualziada exitosamente',
+      data: noticia,
+    }
    }
 }
