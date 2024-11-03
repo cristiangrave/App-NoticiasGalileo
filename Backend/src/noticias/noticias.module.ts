3b90f977f38e11f43d04a8e3fcaeeda0af3d02bd
Micker <carbonellerick811@gmail.com>
2024-11-02 23:52:50 -0600

    Fix: UploadService como Provider en Noticias

--- Backend/src/noticias/noticias.module.ts
+++ Backend/src/noticias/noticias.module.ts
@@ -6,11 +6,12 @@ import { Noticia } from "./entities/noticia.entity";
 import { Categoria } from "./entities/categoria.entity";
 import { Carrera } from "src/carreras/entities/carrera.entity";
 import { Usuario } from "src/users/entities/usuario.entity";
+import { UploadService } from "./services/upload.service";
 
 /** Module para controller y services */
 @Module({
   imports: [TypeOrmModule.forFeature([Noticia, Categoria, Carrera, Usuario])],
   controllers: [NoticiasController],
-  providers: [NoticiaService],
+  providers: [NoticiaService, UploadService],
 })
 export class NoticiasModule {}
